c0333f585df438d7633ef3c0385062d66e3fee41
Nick Schockmel <nick.schockmel@gmail.com>
2022-07-29 17:25:19 -0700

    Update e_note_update.sh

--- update_e_collection_notes/e_note_update.sh
+++ update_e_collection_notes/e_note_update.sh
@@ -1,8 +1,22 @@
 #!bin/bash
 
+#######################################################################################################################
+# This script can be used to update alma electronic collection internal descriptions (notes) with
+# new notes in batch. The script does roughly the following:
+# 1. loops through a txt file of collection id/note pairs
+# 2. uses the vlaues in the coll_id column to make get calls to the alma api and retreive the e-collection xml object
+# 3. updates the internal_description field with the information from the "note" column
+# 4. makes put calls to send the updated e-collection objects back to the alm api
+# 5. creates a txt of coll_id pairs for verification that the put call was successful
+#
+# BEWARE: the put calls will replace the "modification date" and "modified by" fields in the e-collection record with 
+# "alma-api". according to ex libris, this behaivor is a normal bi-product of the get/put swap model.
+#######################################################################################################################
+
+
 cat coll_ids.txt | while IFS= read item
 do
-	# isolate variable from tsv file of collection id/note pairs
+	# isolate variable from tsv file of collection id/note pairs 
 	coll_id="$(cut -f1 <<<$item)"
 	note="$(cut -f2 <<<$item)"
 
